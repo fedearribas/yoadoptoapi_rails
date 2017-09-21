4956d9572caf0c5a2cc4842b88dae1dd336e2c0c
Federico <fede.arribas@gmail.com>
2017-09-21 00:37:07 -0300

    Added seeds for adoptions database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Adoption.create!(
+ name: "Eru",
+ age: 4,
+ age_measurement_unit: "Años",
+ image: "http://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg",
+ adopted: false,
+ description: "Hermoso gatito",
+ published_date: Date.current(),
+ contact_phone: 155117310,
+ contact_email: "test@test.com"
+)
+
+Adoption.create!(
+ name: "Chango",
+ age: 10,
+ age_measurement_unit: "Años",
+ image: "http://librecatamarca.com.ar/press/wp-content/uploads/2014/09/ovejero.jpg",
+ adopted: false,
+ description: "Hermoso perro",
+ published_date: Date.current(),
+ contact_phone: 155117310,
+ contact_email: "test@test.com"
+)
