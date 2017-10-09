0cb1ec9483b2524141e294427cd49785a45606b5
fede.arribas@gmail.com <fede.arribas@gmail.com>
2017-10-09 02:06:59 +0000

    Fixex facebook image size

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], secure_image_url: true
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], secure_image_url: true, :image_size => 'large'
 end
